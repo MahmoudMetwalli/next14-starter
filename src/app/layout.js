7fa3dceb0e0182af0943fe0f03ca9078450f6d79
MahmoudMetwalli <mammkbih@gmail.com>
2024-06-08 12:38:33 +0300

    2024-06-08 12:38:33 PM

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,23 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
+import NavBar from '@/components/navbar/navbar';
+import Footer from '@/components/footer/footer';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Next App',
-  description: 'Next.js starter app',
-}
+  description: 'Next.js starter app'
+};
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import Links from './links/links';
+
+const NavBar = () => {
+  return (
+    <div>NavBar
+      <div>Logo</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
