7779e3d2483c4948b9ae1f29dd9b4aac85e13a03
MahmoudMetwalli <mammkbih@gmail.com>
2024-06-09 02:16:39 +0300

    2024-06-09 02:16:39 AM

--- src/components/navbar/links/links.jsx
+++ src/components/navbar/links/links.jsx
@@ -1,40 +1,55 @@
+'use client';
+
+import { useState } from 'react';
 import styles from './links.module.css';
 import NavLink from './navLink/navLink';
 
+
+const links = [
+  {
+    title: 'Home',
+    path: '/'
+  },
+  {
+    title: 'About',
+    path: '/about'
+  },
+  {
+    title: 'Contact',
+    path: '/contact'
+  },
+  {
+    title: 'Blog',
+    path: '/blog'
+  }
+];
+
+
 const Links = () => {
-  const links = [
-    {
-      title: 'Home',
-      path: '/'
-    },
-    {
-      title: 'About',
-      path: '/about'
-    },
-    {
-      title: 'Contact',
-      path: '/contact'
-    },
-    {
-      title: 'Blog',
-      path: '/blog'
-    }
-  ];
+  const [open, setOpen] = useState(false);
   // TEMPORARY
   const session = true;
   const admin = true;
   return (
-    <div className={styles.links}>
-      {links.map(link => (
-        <NavLink item={link} key={link.title} />
-      ))}
-      {session
-        ? (
-          <>{admin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
-          <button>Log Out</button>
-          </>
-          )
-        : (<NavLink item={{ title: 'LogIn', path: '/login' }} />)}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map(link => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session
+          ? (
+            <>{admin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
+            <button className={styles.logout}>Log Out</button>
+            </>
+            )
+          : (<NavLink item={{ title: 'LogIn', path: '/login' }} />)}
+      </div>
+      <button onClick={() => setOpen(prev => !prev)}>Menu</button>
+      { open && (<div className={styles.mobileLinks}>
+        {links.map(link => (
+          <NavLink item={link} key={link.title} />
+        ))}</div>
+      )}
     </div>
   );
 };
