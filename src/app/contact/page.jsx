6bf1f65b36e60a50d23f84c67e007c86c111bee2
MahmoudMetwalli <mammkbih@gmail.com>
2024-06-14 18:35:31 +0300

    2024-06-14 06:35:31 PM

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,21 @@
+import Image from 'next/image';
+import styles from './contact.module.css'
 const ContactPage = () => {
 	return (
-	  <div>ContactPage</div>
+	  <div className={styles.container}>
+		<div className={styles.imageContainer}>
+			<Image className={styles.img} src='/contact.png' alt='' fill/>
+		</div>
+		<div className={styles.formContainer}>
+			<form action='' className={styles.form}>
+				<input type='text' placeholder='Name'/>
+				<input type='text' placeholder='E-Mail Adress'/>
+				<input type='text' placeholder='Phone Number (Optional)'/>
+				<textarea name='' id='' cols='30' rows='10' placeholder='Please type your message here...'></textarea>
+				<button>Send</button>
+			</form>
+		</div>
+	  </div>
   );
 };
 
