96718e969661186d91434bd059c0da8b77c9241f
MahmoudMetwalli <mammkbih@gmail.com>
2024-06-19 12:31:59 +0300

    2024-06-19 12:31:59 PM

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,6 +1,30 @@
+import Image from 'next/image';
+import styles from './singlePost.module.css'
+
 const SinglePostPage = () => {
 	return (
-	  <div>SinglePostPage</div>
+	  <div className={styles.container}>
+		<div className={styles.imageContainer}>
+			<Image className={styles.image} src='/food2.png' alt='' fill/>
+		</div>
+		<div className={styles.textContainer}>
+			<h1 className={styles.title}>Tilte</h1>
+			<div className={styles.detail}>
+				<Image className={styles.avatar} src='/menu.png' alt='' height={30} width={30}/>
+				<div className={styles.detailText}>
+					<span className={styles.detailTitle}>Author</span>
+					<span className={styles.detailValue}>Snacky</span>
+				</div>
+				<div className={styles.detailText}>
+					<span className={styles.detailTitle}>Published</span>
+					<span className={styles.detailValue}>01.01.2025</span>
+				</div>
+			</div>
+			<div className={styles.content}>
+				Desc
+			</div>
+		</div>
+	  </div>
   );
 };
 
